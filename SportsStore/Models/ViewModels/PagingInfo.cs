ec1df6669e4ad23976f24d0d2992e8f63f91d42d
Delyanaa <delyanaaivanova@gmail.com>
2020-11-05 18:18:33 +0200

    Added TagHelper PageLinkTagHelper v1

--- SportsStore/Models/ViewModels/PagingInfo.cs
+++ SportsStore/Models/ViewModels/PagingInfo.cs
@@ -7,5 +7,11 @@ namespace SportsStore.Models
 {
     public class PagingInfo
     {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        public int TotalPages =>
+        (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
     }
 }
