5bb38f05917043cddb802a10747263eb228e4d4f
Delyanaa <delyanaaivanova@gmail.com>
2020-11-09 15:32:55 +0200

    Added NavigationViewComponent

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        public string Invoke()  //(InvokeAsync) this is the name of the method by convention 
+        {
+            return "This will be the category list";
+        }
+    }
+}
