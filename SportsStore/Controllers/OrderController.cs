a783224b53bc080d2a5ce01065a99cf25bce66bc
Delyanaa <delyanaaivanova@gmail.com>
2020-11-12 23:22:53 +0200

    Modified the constructor

--- SportsStore/Controllers/OrderController.cs
+++ SportsStore/Controllers/OrderController.cs
@@ -10,6 +10,40 @@ namespace SportsStore.Controllers
 {
     public class OrderController : Controller
     {
+        public IOrderRepository orderRepository;
+        public Cart cart;
+
+        public OrderController(IOrderRepository repo, Cart cartServicer)
+        {
+            orderRepository = repo;
+            cart = cartServicer;
+        }
+
         public ViewResult Checkout() => View(new Order());
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (cart.Lines.Count() == 0)
+                ModelState.AddModelError("", "Sorry, your cart is empty");
+
+            if (ModelState.IsValid)
+            {
+                order.Lines = cart.Lines.ToArray();
+                orderRepository.SaveOrder(order);
+
+                return RedirectToAction(nameof(Completed));
+            }
+            else
+            {
+                return View(order);
+            }
+        }
+
+        public ViewResult Completed()
+        {
+            cart.Clear();
+            return View();
+        }
     }
 }
