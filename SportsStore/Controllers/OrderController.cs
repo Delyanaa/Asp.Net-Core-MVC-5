b30e308f9fbe320dc61008599a50caac88302ea8
Delyanaa <delyanaaivanova@gmail.com>
2020-11-12 22:03:12 +0200

    Extended the db: Created Orders TableL

--- SportsStore/Controllers/OrderController.cs
+++ SportsStore/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,6 @@ namespace SportsStore.Controllers
 {
     public class OrderController : Controller
     {
-        public ViewResult Checkout() => ReturnTypeEncoder View(new Order());
+        public ViewResult Checkout() => View(new Order());
     }
 }
